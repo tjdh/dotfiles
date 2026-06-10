f0be17f1d0a4db028c944d9948914356137ab753
Thomas Jeong <dumbhacker123@gmail.com>
2026-06-09 22:16:43 -0400

    added starship config and made install.sh more cross-OS compatible

--- install.sh
+++ install.sh
@@ -1,13 +1,34 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e
 
 DOTFILES="$HOME/dotfiles"
 
-ln -sf "$DOTFILES/tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$DOTFILES/zshrc" "$home/.zshrc"
-ln -sf "$DOTFILES/gitconfig" "$HOME/.gitconfig"
+link() {
+    src="$1"
+    dst="$2"
 
-mkdir -p "$HOME/.config"
-ln -sfn "$DOTFILES/config/nvim" "$HOME/.config/nvim"
+    mkdir -p "$(dirname "$dst")"
 
+    if [ -e "$dst" ] && [ ! -L "$dst" ]; then
+        mv "$dst" "$dst.backup.$(date +%Y%m%d%H%M%S)"
+    fi
+
+    ln -sfn "$src" "$dst"
+}
+
+link "$DOTFILES/tmux.conf" "$HOME/.tmux.conf"
+link "$DOTFILES/zshrc" "$HOME/.zshrc"
+link "$DOTFILES/gitconfig" "$HOME/.gitconfig"
+link "$DOTFILES/config/nvim" "$HOME/.config/nvim"
+link "$DOTFILES/config/starship.toml" "$HOME/.config/starship.toml"
+
+if grep -qi microsoft /proc/version 2>/dev/null; then
+    WIN_HOME="$(wslpath "$(powershell.exe -NoProfile -Command '[Environment]::GetFolderPath("UserProfile")' | tr -d '\r')")"
+
+    rm -rf "$WIN_HOME/.config/wezterm"
+    mkdir -p "$WIN_HOME/.config/wezterm"
+    cp "$DOTFILES/wezterm/wezterm.lua" "$WIN_HOME/.config/wezterm/wezterm.lua"
+else
+    link "$DOTFILES/wezterm" "$HOME/.config/wezterm"
+fi
